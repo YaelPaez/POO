1a7a218871f6baed51b99b74698ff03f2b6fa364
YaelPaez <84360965+YaelPaez@users.noreply.github.com>
2025-03-02 18:02:44 -0600

    Tarea1

--- Ejercicio03.cs
+++ Ejercicio03.cs
@@ -10,25 +10,25 @@ namespace Tarea1
 			double L1, L2, L3;
 			string tipo;
 
-            // Indico el objetivo de la aplicacion y pido los datos
-            // que se requieren
-
-            Console.WriteLine("te dire que tipo de trangulo es");
+		        // Indico el objetivo de la aplicacion y pido los datos
+		        // que se requieren
+		
+		        Console.WriteLine("te dire que tipo de trangulo es");
 
 			Console.WriteLine("Intruduce la medida del primer lado");
 
-            // En este caso va a leer el dato con Console.ReadLine(),
-            // pero como lo declare como tipo double, lo tenemos que
-            // covertir a double con double.Parse(), y el dato ya leido
+            		// En este caso va a leer el dato con Console.ReadLine(),
+            		// pero como lo declare como tipo double, lo tenemos que
+            		// covertir a double con double.Parse(), y el dato ya leido
 			// y convertido a double ahora si se puede almacenar 
 
-            L1 = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Intruduce la medida del segundo lado");
-            L2 = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Intruduce la medida del tercer lado");
-            L3 = double.Parse(Console.ReadLine());
+		        L1 = double.Parse(Console.ReadLine());
+		
+		        Console.WriteLine("Intruduce la medida del segundo lado");
+		        L2 = double.Parse(Console.ReadLine());
+		
+		        Console.WriteLine("Intruduce la medida del tercer lado");
+		        L3 = double.Parse(Console.ReadLine());
 
 			// por medio de un IF ELSE, haho las pruebas logicas
 			// para despues guardar en la var <tipo> el valor a, b o c,
@@ -58,23 +58,23 @@ namespace Tarea1
 			switch (tipo)
 			{
 				case "a":
-					Console.WriteLine("El triangulo es equilatero");
-					break;
-                case "b":
-                    Console.WriteLine("El triangulo es isoceles");
-                    break;
-                case "c":
-                    Console.WriteLine("El triangulo es escaleno");
-                    break;
+				    Console.WriteLine("El triangulo es equilatero");
+				    break;
+		                case "b":
+		                    Console.WriteLine("El triangulo es isoceles");
+		                    break;
+		                case "c":
+		                    Console.WriteLine("El triangulo es escaleno");
+		                    break;
 				default:
-					break;
-            }
+				    break;
+            	       }
 
-			// se espera a que el usuario oprima cualquier tecla para
-			// finalizar la ejecucion de la app
+		       // se espera a que el usuario oprima cualquier tecla para
+		       // finalizar la ejecucion de la app
 
 			Console.ReadKey();
-        }
+        	}
 	}
 }
 
