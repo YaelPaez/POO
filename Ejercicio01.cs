717a5931fe5cf8365872dc783c37dca82dd01723
YaelPaez <84360965+YaelPaez@users.noreply.github.com>
2025-03-02 18:17:17 -0600

    Tarea1

--- Ejercicio01.cs
+++ Ejercicio01.cs
@@ -7,10 +7,23 @@ namespace Tee
     {
         static void Main(string[] args)
         {
+            // Declara variable dia como tipo Int
+            
             int dia;
+
+            // se muestra en pantalla de que se trata la app
+            // y pedimos el dato
+            
             Console.WriteLine("¿Que dia es hoy?");
             Console.WriteLine("ingresa un numero del i al 7");
+            
+            // Leemos la consola Y convertimos el dato a tipo int
+            // para guardarlo en la var dia
+            
             dia = int.Parse(Console.ReadLine());
+
+            // Con el Switch en caso de que el numero ingresadp sea
+            // 1 se mostrara Domigo o en caso de 7 sea Sabado
             switch (dia)
             {
                 case 1:
@@ -34,6 +47,7 @@ namespace Tee
                 case 7:
                     Console.WriteLine("Hoy es Sabado");
                     break;
+                // en caso que sea otro valor diferente
                 default:
                     Console.WriteLine("La opcion no es valida");
                     break;
