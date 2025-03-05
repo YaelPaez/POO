f6ef645dd067ecf1ea6c7812fd845d8acc5de2ce
YaelPaez <84360965+YaelPaez@users.noreply.github.com>
2025-03-05 08:57:00 -0600

    clase08 - Gatito

---
+++ JuegoGato.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gatito
+{
+    internal class JuegoGato
+    {
+        public int[,] Tablero = new int[3,3];
+        public string Jugador;
+
+        public void Volado()
+        {
+            Random rnd = new Random();
+            var resultado = rnd.Next(1, 3);
+            Jugador = (resultado == 1) ? "X" : "0";
+        }
+
+        public void Dibujar()
+        {
+            Console.WriteLine(" -----------");
+            Console.WriteLine("| " + Imagen(0, 0) + " | " + Imagen(0, 1) + " | " + Imagen(0, 2) + " |");
+            Console.WriteLine(string.Format("| {0} | {1} | {2} |", Imagen(1, 0), Imagen(1, 1), Imagen(1, 2)));
+            Console.WriteLine($"| {Imagen(2, 0)} | {Imagen(2, 1)} | {Imagen(2, 2)} |");
+            Console.WriteLine(" -----------");
+            Console.WriteLine($"Juagador actual: {Jugador}");
+        }
+
+        public bool Movimiento()
+        {
+            int x, y;
+            do
+            {
+
+                Console.WriteLine("Escrib3 coordenada X:");
+                x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Escrib3 coordenada Y:");
+                y = Convert.ToInt32(Console.ReadLine());
+            } while (!(x >= 0 && x <= 2) || !(x >= 0 && x <= 2));
+
+            if (Jugador == "X")
+            {
+                Tablero[x, y] = 1;
+                Jugador = "O";
+            }
+            else
+            {
+                Tablero[x, y] = 2;
+                Jugador = "X";
+            }
+
+            return true;
+        }
+
+        public string Imagen(int x, int y)
+        {
+            var valor = Tablero[x,y];
+            switch (valor)
+            {
+                case 0:
+                    return " ";
+                case 1:
+                    return "X";
+                case 2:
+                    return "O";
+                default:
+                    return " ";
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,47 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ciclos
+namespace Gatito
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            bool acierto = false;
-
-            Console.WriteLine("Adivina el numero del 1 al 3");
-
-            bool bandera = true;
+            var juego = new JuegoGato();
+            juego.Volado();
             do
             {
-                var aleatorio = new Random();
-                var num = aleatorio.Next(0, 3);
-
-                Console.WriteLine("Dime el numero:");
-               
-                int opc = int.Parse(Console.ReadLine());
-
-                if (num == opc)
-                    {
-                    acierto = true;
-                   
-                    } else { Console.WriteLine("Intenta de nuevo!"); }
-
-                } while (acierto==false);
-            Console.WriteLine("Has acertado");
+                juego.Dibujar();
+                juego.Movimiento();
+            } while (true);
             Console.ReadKey();
-
-            /*----------------------------------------------------------------------*/
-
-            /*bool bandera = true;
-            while (bandera)
-            {
-                Console.WriteLine("deseas continuar");
-                Console.WriteLine("S/N:");
-                string opc = Console.ReadLine().ToUpper().Trim().Substring(0, 1);
-
-                if (opc == "N") bandera = false;
-            }*/
         }
     }
 }
